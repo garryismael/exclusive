660baac9f68324619d5e15ccab1b75d31f35ee1c
Garry <tahinjanaharygarry@gmail.com>
2024-10-22 07:36:38 +0300

    style new arrival

--- src/app/sections/home/new-arrival/new-arrival.component.ts
+++ src/app/sections/home/new-arrival/new-arrival.component.ts
@@ -1,9 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-arrival',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './new-arrival.component.html',
   styleUrl: './new-arrival.component.css'
 })
