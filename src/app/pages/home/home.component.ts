4560ec426190d07bce20b3dc7d8ec44314fd2e45
Garry <tahinjanaharygarry@gmail.com>
2024-10-22 06:46:23 +0300

    add new arrival section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,17 @@
+import { ProductComponent } from '@/app/components/product/product.component';
+import { BestSellingProductsComponent } from '@/app/sections/home/best-selling-products/best-selling-products.component';
+import { CategoriesComponent } from '@/app/sections/home/categories/categories.component';
+import { FlashSaleBannerComponent } from '@/app/sections/home/flash-sale-banner/flash-sale-banner.component';
+import { FlashSalesProductsComponents } from '@/app/sections/home/flash-sales-products/flash-sales-products.component';
+import { HeroProductsCarouselComponent } from '@/app/sections/home/hero-products-carousel/hero-products-carousel.component';
+import { NewArrivalComponent } from '@/app/sections/home/new-arrival/new-arrival.component';
+import { ProductsSection } from '@/app/sections/home/products/products.component';
 import { Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import {
   faSolidArrowRight,
   faSolidChevronRight,
 } from '@ng-icons/font-awesome/solid';
-import { ProductsSection } from '../../sections/home/products/products.component';
-import { HeroProductsCarouselComponent } from '@/app/sections/home/hero-products-carousel/hero-products-carousel.component';
-import { CategoriesComponent } from '@/app/sections/home/categories/categories.component';
-import { BestSellingProductsComponent } from '@/app/sections/home/best-selling-products/best-selling-products.component';
-import { FlashSaleBannerComponent } from '@/app/sections/home/flash-sale-banner/flash-sale-banner.component';
-import { FlashSalesProductsComponents } from '@/app/sections/home/flash-sales-products/flash-sales-products.component';
-import { ProductComponent } from '../../components/product/product.component';
 
 @Component({
   selector: 'app-home',
@@ -24,6 +25,7 @@ import { ProductComponent } from '../../components/product/product.component';
     FlashSaleBannerComponent,
     ProductComponent,
     ProductsSection,
+    NewArrivalComponent,
   ],
   providers: [provideIcons({ faSolidArrowRight, faSolidChevronRight })],
   templateUrl: './home.component.html',
