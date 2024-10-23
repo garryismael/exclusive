682dce6f63a7baeac871c8c73a192ed66c981dcc
Garry <tahinjanaharygarry@gmail.com>
2024-10-23 12:33:25 +0300

    toggle menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,47 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../../components/logo/logo.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { heroHeart, heroShoppingCart, heroBars3BottomRight } from '@ng-icons/heroicons/outline';
+import {
+  heroHeart,
+  heroShoppingCart,
+  heroBars3BottomRight,
+  heroXMark,
+} from '@ng-icons/heroicons/outline';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [LogoComponent, NgIconComponent, RouterLinkActive, RouterLink],
-  providers: [provideIcons({ heroHeart, heroShoppingCart, heroBars3BottomRight })],
+  imports: [
+    LogoComponent,
+    NgIconComponent,
+    RouterLinkActive,
+    RouterLink,
+    CommonModule,
+  ],
+  providers: [
+    provideIcons({
+      heroHeart,
+      heroShoppingCart,
+      heroBars3BottomRight,
+      heroXMark,
+    }),
+  ],
   standalone: true,
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  active: boolean = false;
+
+  toggle(): void {
+    this.active = !this.active;
+  }
+
+  getNavClasses() {
+    return {
+      active: this.active,
+      inactive: !this.active,
+    };
+  }
+}
